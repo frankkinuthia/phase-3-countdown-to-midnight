a6a054614b653dff05341812c3eff739c5218207
Frank <f.kinuthia01@gmail.com>
2023-03-23 05:17:57 +0300

    Done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
-#write your code here
-
-def countdown
+def countdown(num)
+    while num > 0
+      puts "#{num} SECOND(S)!"
+      num -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+  
+def countdown_with_sleep(num)
+    while num > 0
+      puts "#{num} SECOND(S)!"
+      num -= 1
+      sleep(1)
+    end
+    "HAPPY NEW YEAR!"
+end
+  
+# In this modified version, we've added a call to the `sleep` method at the end of each loop iteration, with an argument of 1, which will pause the program execution for one second on each iteration.
+# The sleep method takes a time argument in seconds and pauses the program execution for that amount of time.
+# Now when you call the countdown method, it will output each number of seconds with a one-second delay between each iteration, giving the appearance of a real countdown.
